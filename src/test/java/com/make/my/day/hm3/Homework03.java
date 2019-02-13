43869fa7c600be685727830b3b824bd99dfe8c8d
Daniil Smirnov <daniil_smirnov@epam.com>
2019-02-14 01:28:40 +0300

    Tasks for hm3

--- src/test/java/com/make/my/day/hm3/Homework03.java
+++ src/test/java/com/make/my/day/hm3/Homework03.java
@@ -1,22 +1,11 @@
 package com.make.my.day.hm3;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import org.junit.Test;
 
@@ -26,7 +15,6 @@ public class Homework03 {
   public void createWithBuilder() {
     // TODO: uncomment and add entities
     Stream<String> sut = null;// = Stream.<String>builder()
-
 
     List<String> resultList = sut.collect(Collectors.toList());
 
@@ -39,8 +27,62 @@ public class Homework03 {
     Stream<Integer> intStream = Stream.of(1, 2);
     Stream<Integer> intStream_2 = Stream.of(3, 4);
     Stream<Integer> intStream_3 = Stream.of(5, 6);
-    
 
-    //assertArrayEquals(new Integer[]{1,2,3,4,5},);
+    // TODO: Concat streams correctly
+    Stream<Integer> prepared = Stream.concat(null, null);
+    Stream<Integer> result = Stream.concat(null, null);
+
+    assertArrayEquals(new Integer[]{1,2,3,4,5,6},result.toArray());
   }
+
+  @Test
+  public void iterateForNineHundredsElements() {
+    // TODO: Add correctly realization of iterate
+    Stream<Integer> stream = Stream.iterate(0, null);
+
+    Integer[] expected = new Integer[900];
+    for (int i = 100, j = 0; j < 900; i++, j++) {
+      expected[j] = i;
+    }
+
+    assertArrayEquals(expected, stream.toArray());
+  }
+
+  @Test
+  public void createWithArraysMethod() {
+    // TODO: Create realization with Arrays.stream
+    IntStream sut = null;
+
+    assertArrayEquals(new int[]{'t', 'u', 'r', 't', 'l', 'e'}, sut.toArray());
+  }
+
+  private class Agent{
+    private final String name = "Smith";
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      Agent agent = (Agent) o;
+      return Objects.equals(name, agent.name);
+    }
+  }
+
+  @Test
+  public void provideStreamWithGenerate() {
+    // TODO: Generate 3000 agents
+    Stream<Agent> agents = Stream.generate(null);
+
+    Agent[] expected = new Agent[3000];
+    for (int i = 0; i < 3000; i++) {
+      expected[i] = new Agent();
+    }
+
+    assertArrayEquals(expected, agents.toArray());
+  }
+
 }
