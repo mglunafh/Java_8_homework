f211921db4df00024d43f5984281ee864322698a
Daniil Smirnov <daniil_smirnov@epam.com>
2019-02-25 19:15:47 +0300

    new tasks

--- src/test/java/com/make/my/day/hm4/Homework04.java
+++ src/test/java/com/make/my/day/hm4/Homework04.java
@@ -75,4 +75,72 @@ public class Homework04 {
 
     assertEquals("Materials[ Glass, Steel, Wood, Stone ]", result);
   }
+
+  @Test
+  public void groupingByWordsLength() {
+    String[] words = new String[]{"one", "one", "one", "two", "two", "three"};
+
+    Map<Integer, List<String>> result = Arrays.stream(words)
+        // TODO: Use here grouping by
+        .collect(null);
+
+    Map<Integer, List<String>> expected = new HashMap<>();
+    expected.put(3, Arrays.asList("one", "one", "one", "two", "two"));
+    expected.put(5, Arrays.asList("three"));
+
+    assertEquals(expected, result);
+  }
+
+
+  private class Dog{
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+      this.name = name;
+      this.age = age;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public int getAge() {
+      return age;
+    }
+
+    public void setAge(int age) {
+      this.age = age;
+    }
+  }
+
+
+  @Test
+  public void dogMappingByAge() {
+    List<Dog> dogs = Arrays.asList(
+        new Dog("Bim", 4), new Dog("Duke", 7), new Dog("Fenrir", 120),
+        new Dog("Bim", 8), new Dog("Lucky", 6), new Dog("Duke", 13));
+
+    Map<String, List<Integer>> result = dogs.stream()
+        // TODO: Use here `groupingBy` plus `mapping`
+        .collect(null);
+
+    Map<String, List<Integer>> expected = new HashMap<>();
+    expected.put("Bim", Arrays.asList(4, 8));
+    expected.put("Duke", Arrays.asList(7, 13));
+    expected.put("Fenrir", Arrays.asList(120));
+    expected.put("Lucky", Arrays.asList(6));
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void partitionByOddEven() {
+    int[] numbers = new int[]{2, 3, 8, 7, 8, 9, 13, 11};
+
+  }
 }
